829001b1f29282bef004f7baa49367f2c3e0d937
Kensaigm <scott@beancurvestudio.com>
2015-05-05 14:38:17 -0700

    Customer Unit Tests Added

---
+++ Test Projects/CustomerTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ACM.BL;
+
+namespace ACM.BLTests
+{
+    [TestClass]
+    public class CustomerTests
+    {
+        [TestMethod]
+        public void CalculatePercentOfGoalStepsTestValid()
+        {
+            // Arrange
+            Customer customer = new Customer();
+            string goalSteps = "5000";
+            string actualSteps = "2000";
+            decimal expected = 40M;
+
+            // Act
+            var actual = customer.CalculatePercentOfGoalSteps(goalSteps, actualSteps);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void CalculatePercentOfGoalStepsTestValidAndSame()
+        {
+            // Arrange
+            Customer customer = new Customer();
+            string goalSteps = "5000";
+            string actualSteps = "5000";
+            decimal expected = 100M;
+
+            // Act
+            var actual = customer.CalculatePercentOfGoalSteps(goalSteps, actualSteps);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void CalculatePercentOfGoalStepsTestValidActualIsZero()
+        {
+            // Arrange
+            Customer customer = new Customer();
+            string goalSteps = "5000";
+            string actualSteps = "0";
+            decimal expected = 0M;
+
+            // Act
+            var actual = customer.CalculatePercentOfGoalSteps(goalSteps, actualSteps);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+
+    }
+}
