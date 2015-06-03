50d52196a7c448aec56ef6ba8aef5b509feeb9a4
Kensaigm <scott@beancurvestudio.com>
2015-06-03 14:47:36 -0700

    Modified ProductRepository class.

---
+++ ACM.BL/ProductRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    class ProductRepository
+    {
+        /// <summary>
+        /// Saves the current product.
+        /// </summary>
+        /// <param name="productId"></param>
+        /// <returns></returns>
+        public Product Retrieve(int productId)
+        {
+            // Code that retrieves the defined product
+            return new Product();
+        }
+
+        /// <summary>
+        /// Saves the current product.
+        /// </summary>
+        /// <returns></returns>
+        public bool Save()
+        {
+            // Code that saves the defined product
+            return true;
+        }
+    }
+}
