e1b4c2fb12ca592a2e0afc9ae8ba8afec5fbe5ac
Kensaigm <scott@beancurvestudio.com>
2015-06-03 18:24:25 -0700

    Updated CustomerRepositoryTest

--- Test Projects/CustomerRepositoryTest.cs
+++ Test Projects/CustomerRepositoryTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ACM.BL;
 
 namespace ACM.BLTests
 {
@@ -9,6 +10,22 @@ namespace ACM.BLTests
         [TestMethod]
         public void RetrieveExisting()
         {
+            //-- Arrange
+            var customerRepository = new CustomerRepository();
+            var expected = new Customer()
+            {
+                CustomerId = 1,
+                EmailAddress = "fbaggins@hobbiton.me",
+                FirstName = "Frodo",
+                LastName = "Baggins"
+            };
+
+            //-- Act
+            var actual = customerRepository.Retrieve(1);
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+
         }
     }
 }
