d0c395c343ad155c3ed2b4f29bb79f14035a6166
Kensaigm <scott@beancurvestudio.com>
2015-06-12 15:38:44 -0700

    Refactor OrderControllerTests to test Email and Customer.

--- Test Projects/OrderControllerTests.cs
+++ Test Projects/OrderControllerTests.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using ACM.BL;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Core.Common;
 
 namespace ACM.BLTests
 {
@@ -14,7 +15,58 @@ namespace ACM.BLTests
         [TestMethod]
         public void PlaceOrderTest()
         {
-            Assert.Fail();
+            //-- Arrange
+            var orderController = new OrderController();
+            var customer = new Customer() { EmailAddress = "scott@beancurvestudio.com" };
+            var order = new Order();
+            var payment = new Payment() { PaymentType = 1 };
+
+            //-- Act
+            OperationResult op = orderController.PlaceOrder(customer, order, payment,
+                allowSplitOrders: true, emailReceipt: true);
+
+            //-- Assert
+            Assert.AreEqual(true, op.Success);
+            Assert.AreEqual(0, op.MessageList.Count);
+
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void PlaceOrderTestNullCustomer()
+        {
+            //-- Arrange
+            var orderController = new OrderController();
+            Customer customer = null;
+            var order = new Order();
+            var payment = new Payment() { PaymentType = 1 };
+
+            //-- Act
+            OperationResult op = orderController.PlaceOrder(customer, order, payment,
+                allowSplitOrders: true, emailReceipt: true);
+
+            //-- Assert
+            // Exception should occure with customer == null
+        }
+
+        [TestMethod()]
+        public void PlaceOrderTestInvalidEmail()
+        {
+            //-- Arrange
+            var orderController = new OrderController();
+            var customer = new Customer() { EmailAddress = "" };
+            var order = new Order();
+            var payment = new Payment() { PaymentType = 1 };
+
+            //-- Act
+            OperationResult op = orderController.PlaceOrder(customer, order, payment,
+                allowSplitOrders: true, emailReceipt: true);
+
+            //-- Assert
+            Assert.AreEqual(true, op.Success);
+            Assert.AreEqual(1, op.MessageList.Count);
+            Assert.AreEqual("Email address is null", op.MessageList[0]);
+        
         }
     }
 }
