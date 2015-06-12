ffc1d0d1778ea744042c769b12974326027851b4
Kensaigm <scott@beancurvestudio.com>
2015-06-11 18:50:16 -0700

    Refactor OrderWin to address status not show when busy.

--- ACM.Win/OrderWin.cs
+++ ACM.Win/OrderWin.cs
@@ -20,6 +20,11 @@ namespace ACM.Win
 
         private void button1_Click(object sender, EventArgs e)
         {
+            Button button = sender as Button;
+            if (button != null)
+            {
+                button.Text = "Processing ...";
+            }
             PlaceOrder();
         }
 
